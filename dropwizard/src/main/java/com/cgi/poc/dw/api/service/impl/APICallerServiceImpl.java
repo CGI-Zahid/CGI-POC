ca2c4ec258b5a16a9c72c952284d2aa44c4c077c
vbaylly-cgi <vincent.baylly@cgi.com>
2017-02-16 15:46:12 -0500

    Fix PR comment

--- dropwizard/src/main/java/com/cgi/poc/dw/api/service/impl/APICallerServiceImpl.java
+++ dropwizard/src/main/java/com/cgi/poc/dw/api/service/impl/APICallerServiceImpl.java
@@ -1,9 +1,7 @@
 package com.cgi.poc.dw.api.service.impl;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Iterator;
 
 import javax.ws.rs.client.Client;
@@ -16,10 +14,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.context.internal.ManagedSessionContext;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,6 +26,7 @@ import com.cgi.poc.dw.dao.model.FireEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
+import com.sun.el.parser.ParseException;
 
 /**
  * class to call API to collect the resources event information
@@ -39,14 +37,15 @@ import com.google.inject.assistedinject.Assisted;
 public class APICallerServiceImpl implements APICallerService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(APICallerServiceImpl.class);
-	
+
 	private Client client;
 	private String eventUrl;
 	private FireEventDAO fireEventDAO;
 	private SessionFactory sessionFactory;
-	
+
 	@Inject
-	public APICallerServiceImpl(@Assisted String eventUrl, @Assisted Client client, @Assisted FireEventDAO fireEventDAO, SessionFactory sessionFactory){
+	public APICallerServiceImpl(@Assisted String eventUrl, @Assisted Client client, @Assisted FireEventDAO fireEventDAO,
+			SessionFactory sessionFactory) {
 		this.eventUrl = eventUrl;
 		this.client = client;
 		this.fireEventDAO = fireEventDAO;
@@ -66,19 +65,14 @@ public class APICallerServiceImpl implements APICallerService {
 
 		LOGGER.debug("json : " + response.getEntity());
 
-		BufferedReader br = new BufferedReader(new InputStreamReader((InputStream) (response.getEntity())));
+		// create ObjectMapper instance
+		ObjectMapper objectMapper = new ObjectMapper();
 
-		JSONParser parser = new JSONParser();
-
-		Object obj = null;
 		try {
-			obj = parser.parse(br);
-			JSONObject eventJson = (JSONObject) obj;
+			JSONObject eventJson = objectMapper.readValue(((InputStream) response.getEntity()), JSONObject.class);
 
 			parsingEventsResponse(eventJson);
 		} catch (IOException e) {
-			LOGGER.error("Unable to parse the result for the url event : {} error: {}", eventUrl, e.getMessage());
-		} catch (ParseException e) {
 			LOGGER.error("Unable to parse the result for the url event : {} error: {}", eventUrl, e.getMessage());
 		}
 	}
@@ -86,47 +80,54 @@ public class APICallerServiceImpl implements APICallerService {
 	/**
 	 * parsing the json to a java object.
 	 * 
-	 * @param featureJson the  json string get from the Rest API
+	 * @param featureJson
+	 *            the json string get from the Rest API
 	 */
-	
-	@SuppressWarnings("unchecked")
-	private void parsingEventsResponse(JSONObject featureJson) throws IOException{
+	private void parsingEventsResponse(JSONObject featureJson) throws IOException {
 
-		JSONArray features = (JSONArray) featureJson.get("features");
-		Iterator<Object> iterator = features.iterator();
-		while (iterator.hasNext()) {
-			
-			JSONObject feature = (JSONObject) iterator.next();
+		try {
 
-	        JSONObject eventJson = (JSONObject)feature.get("attributes");
-	        JSONObject geoJson = (JSONObject)feature.get("geometry");
-	        ObjectMapper mapper = new ObjectMapper();
-	        FireEvent event = mapper.readValue(eventJson.toString(), FireEvent.class);
+			JSONArray featuresArray = (JSONArray) featureJson.get("features");
+			JSONArray featureArrayJson = featuresArray.getJSONArray(1);
 
-			event.setGeometry(geoJson.toJSONString());
-			
-	        Session session = sessionFactory.openSession();
-	        try {
-	            ManagedSessionContext.bind(session);
-	            Transaction transaction = session.beginTransaction();
-	            try {
-	            	
-	                // Archive users based on last login date
-	            	fireEventDAO.save(event);
-	                transaction.commit();
-	            }
-	            catch (Exception e) {
-	                transaction.rollback();
-	                throw new RuntimeException(e);
-	            }
-	        } finally {
-	            session.close();
-	            ManagedSessionContext.unbind(sessionFactory);
-	        }
+			for (int i = 0; i > featureJson.length(); i++) {
 
-			LOGGER.info("Event to save : {}", event.toString());
+				JSONObject feature;
 
+				feature = (JSONObject) featureArrayJson.get(i);
+
+				JSONObject eventJson = (JSONObject) feature.get("attributes");
+				JSONObject geoJson = (JSONObject) feature.get("geometry");
+				ObjectMapper mapper = new ObjectMapper();
+				FireEvent event = mapper.readValue(eventJson.toString(), FireEvent.class);
+
+				event.setGeometry(geoJson.toString());
+
+				Session session = sessionFactory.openSession();
+				try {
+					ManagedSessionContext.bind(session);
+					Transaction transaction = session.beginTransaction();
+					try {
+
+						// Archive users based on last login date
+						fireEventDAO.save(event);
+						transaction.commit();
+					} catch (Exception e) {
+						transaction.rollback();
+						throw new RuntimeException(e);
+					}
+				} finally {
+					session.close();
+					ManagedSessionContext.unbind(sessionFactory);
+				}
+
+				LOGGER.info("Event to save : {}", event.toString());
+
+			}
+
+		} catch (JSONException e) {
+			LOGGER.error("Unable to parse the result for the url event : {} error: {}", eventUrl, e.getMessage());
 		}
 	}
-	
+
 }
