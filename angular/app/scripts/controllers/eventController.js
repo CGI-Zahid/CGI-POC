2f71970e0472900da37f8fee02317a03e370ffd9
cjekal <charles.jekal@gmail.com>
2017-02-20 11:42:56 -0800

    added the eventController to the index

--- angular/app/scripts/controllers/eventController.js
+++ angular/app/scripts/controllers/eventController.js
@@ -4,7 +4,11 @@ cgiWebApp.controller('eventController',
   ['$scope', '$sessionStorage', '$state',
   function ($scope, $sessionStorage, $state) {
 
+  $scope.notification = {
+    messageCharsRemaining: 135
+  };
+
   $scope.publishEvent = function() {
     $sessionStorage.put('state', $state.current.name);
-  }
+  };
 }]);
